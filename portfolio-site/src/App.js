283f369bc22b80cd35f2bcff4e91066a726a6d53
maccarlton25 <39240058+maccarlton25@users.noreply.github.com>
2021-09-20 14:56:50 -0400

    initial commit

---
+++ portfolio-site/src/App.js
@@ -0,0 +1,11 @@
+import Landing from './components/Landing.jsx';
+
+function App() {
+  return (
+      <Landing/>
+  );
+}
+
+export default App;
+
+//https://css-tricks.com/how-to-map-mouse-position-in-css/
---
+++ portfolio-site/src/components/Landing.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+import '../css/Landing.scss'
+
+function Landing() {
+
+    return (
+        <div className="landingWrapper">
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+
+            <div className="content">
+                <div className="text"></div>
+            </div>
+            <div className="homeButton">
+            <a className="goingHome" href="https://www.w3schools.com">Lets get started!</a>
+            </div>
+        </div>
+    );
+}
+
+export default Landing;
